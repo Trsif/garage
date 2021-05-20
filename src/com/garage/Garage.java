5cf77ade895badb559b95431634d606ecf2496c4
trsif <pepefava1@gmail.com>
2021-05-20 19:59:19 -0400

    added garage

--- src/com/garage/Garage.java
+++ src/com/garage/Garage.java
@@ -2,21 +2,45 @@ package com.garage;
 
 import java.util.ArrayList;
 
-public class Garage {
-	private ArrayList<String> vehiclesIn = new ArrayList<>();
+public class Garage extends Vehicle {
+	private String name;
+	private ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
 
-	public ArrayList<String> getVehicles() {
-		return vehiclesIn;
+	public ArrayList<Vehicle> getVehicles() {
+		return vehicles;
 	}
 
-	public void setVehicles(ArrayList<String> vehicles) {
-		this.vehiclesIn = vehicles;
+	public void setVehicles(ArrayList<Vehicle> vehicles) {
+		this.vehicles = vehicles;
 	}
 
-	public void addVehicle(String... vehicles) {
-		for (String i : vehicles) {
-			vehiclesIn.add(i);
+	public void addVehicle(Vehicle... vehicle) {
+		for (Vehicle i : vehicle) {
+			vehicles.add(i);
 		}
+
+	}
+
+	public void vehicles() {
+
+		for (int i = 0; i < vehicles.size(); i++) {
+			System.out.print(vehicles.get(i).toString());
+		}
+	}
+
+	String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	String getParts() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/com/garage/Runner.java
+++ src/com/garage/Runner.java
@@ -4,14 +4,16 @@ public class Runner {
 
 	public static void main(String[] args) {
 		Garage garage = new Garage();
-		Car fam = new Car();
-		fam.setName("Fahira");
-		garage.addVehicle(fam.getName());
+		Car c = new Car();
+		Boat b = new Boat();
+		Moto m = new Moto();
+		garage.addVehicle();
+		c.setName("Fahira");
+		b.setName("BoatTrue");
+		garage.addVehicle(c, b, m);
+		System.out.println(b.getName());
+		garage.vehicles();
 
-		String s = fam.getType();
-		System.out.println(s);
-		System.out.println(fam.getName());
-		System.out.println(garage.getVehicles());
 	}
 
 }
